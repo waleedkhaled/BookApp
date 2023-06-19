be6f1f15746e30796bd1e191fd88fe3304c39efb
waleed khaled <waleedmini2@gmail.com>
2023-06-18 21:34:00 -0700

    :tada: intiial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Book-system-management
---
+++ src/screens/home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+
+const Home = () => {
+
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+        flex: 1
+    },
+});
+
+export default Home;
